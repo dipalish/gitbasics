11b0fa468bae4bb0d3d301c36c520287205c2db3
Dipali Sharma <sharmdipali@gmail.com>
2023-07-27 22:23:13 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,11 @@
+public class Sum {
+ 
+    public static void main(String[] args)
+    {
+        int a = 62;
+        int b = 18;
+        // It will return the sum of two arguments.
+        System.out.println("The sum is ="
+                           + Integer.sum(a, b));
+    }
+}
